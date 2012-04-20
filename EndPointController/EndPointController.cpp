c948c06e98f622ea68a9dd60b5f4289e9bb5d5db
Daniel Stevens <daniel.stevens@rightnow.com>
2012-04-20 13:58:56 +0100

    Refactored code to replace deep-nested ifs with function calls.

--- EndPointController/EndPointController.cpp
+++ EndPointController/EndPointController.cpp
@@ -9,7 +9,113 @@
 #include "Propidl.h"
 #include "Functiondiscoverykeys_devpkey.h"
 
+// Format string for outputing a device entry. The following parameters will be used in the following order:
+// Index, Device Friendly Name
 #define DEVICE_OUTPUT_FORMAT "Audio Device %d: %ws\n"
+
+typedef struct TGlobalState
+{
+	HRESULT hr;
+	int option;
+	IMMDeviceEnumerator *pEnum;
+	IMMDeviceCollection *pDevices;
+	IMMDevice *pCurrentDevice;
+} TGlobalState;
+
+void createDeviceEnumerator(TGlobalState* state);
+void prepareDeviceEnumerator(TGlobalState* state);
+void enumerateOutputDevices(TGlobalState* state);
+HRESULT printDeviceInfo(IMMDevice* pDevice, int index);
+HRESULT SetDefaultAudioPlaybackDevice(LPCWSTR devID);
+
+// EndPointController.exe [NewDefaultDeviceID]
+int _tmain(int argc, _TCHAR* argv[])
+{
+	TGlobalState state;
+	
+	// read the command line option, -1 indicates list devices.
+	state.option = -1;
+	if (argc == 2) state.option = atoi((char*)argv[1]);
+
+	state.hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED);
+	if (SUCCEEDED(state.hr))
+	{
+		createDeviceEnumerator(&state);
+	}
+	return state.hr;
+}
+
+// Create a multimedia device enumerator.
+void createDeviceEnumerator(TGlobalState* state)
+{
+	state->pEnum = NULL;
+	state->hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL, CLSCTX_ALL, __uuidof(IMMDeviceEnumerator),
+		(void**)&state->pEnum);
+	if (SUCCEEDED(state->hr))
+	{
+		prepareDeviceEnumerator(state);
+	}
+}
+
+// Prepare the device enumerator
+void prepareDeviceEnumerator(TGlobalState* state)
+{
+	state->hr = state->pEnum->EnumAudioEndpoints(eRender, DEVICE_STATE_ACTIVE, &state->pDevices);
+	if SUCCEEDED(state->hr)
+	{
+		enumerateOutputDevices(state);
+	}
+	state->pEnum->Release();
+}
+
+// Enumerate the output devices
+void enumerateOutputDevices(TGlobalState* state)
+{
+	UINT count;
+	state->pDevices->GetCount(&count);
+
+	for (int i = 0; i < (int)count; i++)
+	{
+		state->hr = state->pDevices->Item(i, &state->pCurrentDevice);
+		if (SUCCEEDED(state->hr))
+		{
+			LPWSTR wstrID = NULL;
+			state->hr = state->pCurrentDevice->GetId(&wstrID);
+			if (SUCCEEDED(state->hr))
+			{
+				// if no options, print the device
+				// otherwise, find the selected device and set it to be default
+				if (state->option == -1) {
+					state->hr = printDeviceInfo(state->pCurrentDevice, i);
+				} else if (i == state->option) {
+					state->hr = SetDefaultAudioPlaybackDevice(wstrID);
+				}
+			}
+			state->pCurrentDevice->Release();
+		}
+	}
+	state->pDevices->Release();
+}
+
+HRESULT printDeviceInfo(IMMDevice* pDevice, int index)
+{
+	IPropertyStore *pStore;
+	HRESULT hr = pDevice->OpenPropertyStore(STGM_READ, &pStore);
+
+	if (SUCCEEDED(hr))
+	{
+		PROPVARIANT friendlyName;
+		PropVariantInit(&friendlyName);
+		hr = pStore->GetValue(PKEY_Device_FriendlyName, &friendlyName);
+		if (SUCCEEDED(hr))
+		{
+			printf(DEVICE_OUTPUT_FORMAT, index, friendlyName.pwszVal);
+			PropVariantClear(&friendlyName);
+		}
+		pStore->Release();
+	}
+	return hr;
+}
 
 HRESULT SetDefaultAudioPlaybackDevice(LPCWSTR devID)
 {	
@@ -22,71 +128,6 @@ HRESULT SetDefaultAudioPlaybackDevice(LPCWSTR devID)
 	{
 		hr = pPolicyConfig->SetDefaultEndpoint(devID, reserved);
 		pPolicyConfig->Release();
-	}
-	return hr;
-}
-
-// EndPointController.exe [NewDefaultDeviceID]
-int _tmain(int argc, _TCHAR* argv[])
-{
-	// read the command line option, -1 indicates list devices.
-	int option = -1;
-	if (argc == 2) option = atoi((char*)argv[1]);
-
-	HRESULT hr = CoInitialize(NULL);
-	if (SUCCEEDED(hr))
-	{
-		IMMDeviceEnumerator *pEnum = NULL;
-		// Create a multimedia device enumerator.
-		hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL,
-			CLSCTX_ALL, __uuidof(IMMDeviceEnumerator), (void**)&pEnum);
-		if (SUCCEEDED(hr))
-		{
-			IMMDeviceCollection *pDevices;
-			// Enumerate the output devices.
-			hr = pEnum->EnumAudioEndpoints(eRender, DEVICE_STATE_ACTIVE, &pDevices);
-			if (SUCCEEDED(hr))
-			{
-				UINT count;
-				pDevices->GetCount(&count);
-				if (SUCCEEDED(hr))
-				{
-					for (int i = 0; i < count; i++)
-					{
-						IMMDevice *pDevice;
-						hr = pDevices->Item(i, &pDevice);
-						if (SUCCEEDED(hr))
-						{
-							LPWSTR wstrID = NULL;
-							hr = pDevice->GetId(&wstrID);
-							if (SUCCEEDED(hr))
-							{
-								IPropertyStore *pStore;
-								hr = pDevice->OpenPropertyStore(STGM_READ, &pStore);
-								if (SUCCEEDED(hr))
-								{
-									PROPVARIANT friendlyName;
-									PropVariantInit(&friendlyName);
-									hr = pStore->GetValue(PKEY_Device_FriendlyName, &friendlyName);
-									if (SUCCEEDED(hr))
-									{
-										// if no options, print the device
-										// otherwise, find the selected device and set it to be default
-										if (option == -1) printf(DEVICE_OUTPUT_FORMAT,i, friendlyName.pwszVal);
-										if (i == option) SetDefaultAudioPlaybackDevice(wstrID);
-										PropVariantClear(&friendlyName);
-									}
-									pStore->Release();
-								}
-							}
-							pDevice->Release();
-						}
-					}
-				}
-				pDevices->Release();
-			}
-			pEnum->Release();
-		}
 	}
 	return hr;
 }
