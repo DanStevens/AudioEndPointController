c8dbba4a5507bdf8bd0aa78db6cedf09e17f4b8d
Daniel Stevens <dan.stevens@oracle.com>
2012-05-05 11:18:18 +0100

    Documented code

--- AudioDeviceController/AudioDevice.h
+++ AudioDeviceController/AudioDevice.h
@@ -19,37 +19,69 @@ namespace AudioDeviceApi
 		enum Enum { UNKNOWN, PLAYBACK, RECORDING };
 	}
 	
+	/**
+	 * Represents an Audio End-point Device on a Windows Vista or Windows 7 system. The class acts as a wrapper for
+	 * an IMMDevice instance, providing more convienient access to the properties and status of the IMMDevice object.
+	 * Instances of this class can exist indepentantly from the IMMDevice, as the original values are copied from the
+	 * IMMDevice and are affectively cached; the properties of the AudioDevice can still be accessed after the 
+	 * IMMDevice used to initialize it has been released.
+	 *
+	 * See http://msdn.microsoft.com/en-us/library/dd371395%28v=vs.85%29.aspx for more information in the IMMDevice
+	 * interface.
+	 */
 	class AudioDevice
 	{
 
 	public:
 		
-		//AudioDevice(LPCWSTR id);
-		//AudioDevice(IMMDevice* pMMDevice);
+		/**
+		 * Constructs a new AudioDevice object from the given IMMDevice.
+		 * \param ppMMDevice a pointer to a pMMDevice to create the AudioDevice object from.
+		 * \param The type of AudioDevice being created. Note that it is not possible to infer the type of device
+		 *        from IMMDevice object, hence this property.
+		 */
 		AudioDevice(IMMDevice* pMMDevice, DeviceType::Enum type = DeviceType::UNKNOWN);
+		
+		//! Deconstructor
 		~AudioDevice();
+		
+		//! Returns the unique Endpoint ID string for the AudioDevice (see http://msdn.microsoft.com/en-us/library/dd370837%28v=vs.85%29.aspx).
 		wstring GetID()
 		{
 			return id;
 		}
+
+		//! Returns the state of the AudioDevice, if known.
 		DeviceState::Enum GetState()
 		{
 			return state;
 		}
+
+		//! Returns the type of the AudioDevice, if known.
 		DeviceType::Enum GetType()
 		{
 			return type;
 		}
+
+		//! Returns the name of the AudioDevice (equivalent to the PKEY_DeviceInterface_FriendlyName property of the 
+		//! IMMDevice).
 		wstring GetName()
 		{
 			return name;
 		}
+
+		//! Returns the name of the AudioDevice (equivalent to the PKEY_Device_DeviceDesc property of the 
+		//! IMMDevice).
 		wstring GetDescription()
 		{
 			return description;
 		}
-		//void SetAsDefault();
 
+		/**
+		 * Tests the passed HRESULT and throws an HResultFailException if it is anything except success.
+		 * \param hr The HRESULT to test.
+		 * \return true if the HRESULT does not indicate a failure.
+		 */
 		static bool testHResult(HRESULT hr)
 		{
 			if (!SUCCEEDED(hr)) {
@@ -59,13 +91,10 @@ namespace AudioDeviceApi
 			}
 		}
 
-		
-
 	protected:
 		wstring id;
 		DeviceState::Enum state;
 		DeviceType::Enum type;
-		//int isDefault;
 		wstring name;
 		wstring description;
 
--- AudioDeviceController/AudioDeviceController.h
+++ AudioDeviceController/AudioDeviceController.h
@@ -21,27 +21,99 @@ namespace AudioDeviceApi
 		}
 	};
 	
+	//! A shared pointer to an AudioDevice object.
 	typedef tr1::shared_ptr<AudioDevice> AudioDevicePtr;
+
+	//! A shared key-value pair, with a wstring as the key and a AudioDevicePtr as the value. 
 	typedef pair<wstring, AudioDevicePtr> IDAudioDevicePair;
+
+	//! A map for storing a list of AudioDevicePtr, using the ID of the device as the key.
 	typedef map<wstring, AudioDevicePtr, IDComparison> AudioDeviceMap;
+
+	//! A shared pointer to an AudioDeviceMap object.
 	typedef tr1::shared_ptr<AudioDeviceMap>AudioDeviceMapPtr;
-	//typedef tr1::shared_ptr<IMMDeviceEnumerator> IMMDeviceEnumeratorPtr;
-	//typedef tr1::shared_ptr<IMMDeviceCollection> IMMDeviceCollectionPtr;
 
-
+	/**
+	 * The AudioDeviceController class provides access Audio End-point Devices of the Windows Vista or Windows 7.
+	 * Consumers can request a list of Audio Devices, and get/set the default audio device. By default,
+	 * AudioDeviceController deals with Playback devices, but it can also be used to retrieve Recording devices and
+	 * get/set the default Recording device by setting parameters of the appropriate function.
+	 */
 	class AudioDeviceController
 	{
 
 	public:
+		
+		/** 
+		 * Constructs a new AudioDeviceController.
+		 * \param dataFlow The data-flow direction for the endpoint devices to deal with. Use 'eRender' for Playback
+		 *                 devices, 'eCapture' for Recording devices and 'eAll' for both Playbacka dn Recording
+		 *                 devices.
+		 * \param dwStateMask The state or states of the endpoints The caller should set this parameter to the bitwise
+		 *                    OR of one or more of the following DEVICE_STATE_XXX constants:
+		 *                      * DEVICE_STATE_ACTIVE
+		 *                      * DEVICE_STATE_DISABLED
+		 *                      * DEVICE_STATE_NOTPRESENT
+		 *                      * DEVICE_STATE_UNPLUGGED
+		 *                    The parameter can be set to DEVICE_STATEMASK_ALL to include all endpoints, regardless of
+		 *                    state. See http://msdn.microsoft.com/en-us/library/dd371400%28v=vs.85%29.aspx for more
+		 *                    information.
+		 */ 
 		AudioDeviceController(EDataFlow dataFlow = eRender, DWORD dwStateMask = DEVICE_STATE_ACTIVE);
-		~AudioDeviceController();
-		int GetCount();
-		AudioDeviceMapPtr GetDevices();
-		AudioDevicePtr GetDefaultDevice(ERole role = eMultimedia, EDataFlow dataFlow = (EDataFlow)0);
-		void SetDefaultDevice(AudioDevicePtr audioDevice, ERole role);
-		void SetDefaultDevice(wstring deviceID, ERole role);
-		void SetDefaultDevice(LPCWSTR deviceID, ERole role);
 
+		/**
+		 * Deconstructor.
+		 */
+		~AudioDeviceController();
+		
+		/**
+		 * Returns the number of devices that correspond with the criteria specified in the constructor.
+		 */
+		int GetCount();
+
+		/**
+		 * Returns a pointer to a map of AudioDevices that correspond with the criteria specifiedin the constructor,
+		 * keyed by the ID of the device.
+		 */
+		AudioDeviceMapPtr GetDevices();
+
+		/**
+		 * Returns a pointer to an instance of AudioDevice that represents the default audio device.
+		 * \param role The Role of the Audio Device. See http://msdn.microsoft.com/en-us/library/dd370813%28v=vs.85%29.aspx
+		 *             The support for roles may depend on the version of Windows.
+		 * \param dataFlow The data-flow direction of the endpoint device. If this is set to 0 (as per default), 
+		 *                 the direction given in the constructor will be assumed. Note that eAll is not a valid value
+		 *                 for this parameter and so 'eRender' is assumed in such case that it is specified here or in
+		 *                 the constructor.
+		 */
+		AudioDevicePtr GetDefaultDevice(ERole role = eMultimedia, EDataFlow dataFlow = (EDataFlow)0);
+
+		/**
+		 * Sets the default Audio Endpoint device to the given AudioDevice.
+		 * \param audioDevice a pointer to the AudioDevice to set as the default.
+		 * \param the role to set default Audio Endpoint device for.
+		 */
+		void SetDefaultDevice(AudioDevicePtr audioDevice, ERole role = eMultimedia);
+
+		/**
+		 * Sets the default Audio Endpoint device to the device with the given ID, as a wstring
+		 * \param audioDevice the ID of the device to set to default.
+		 * \param the role to set default Audio Endpoint device for.
+		 */
+		void SetDefaultDevice(wstring deviceID, ERole role = eMultimedia);
+
+		/**
+		 * Sets the default Audio Endpoint device to the device with the given ID, as a LPCWSTR
+		 * \param audioDevice the ID of the device to set to default.
+		 * \param the role to set default Audio Endpoint device for.
+		 */
+		void SetDefaultDevice(LPCWSTR deviceID, ERole role = eMultimedia);
+
+		/**
+		 * Tests the passed HRESULT and throws an HResultFailException if it is anything except success.
+		 * \param hr The HRESULT to test.
+		 * \return true if the HRESULT does not indicate a failure.
+		 */
 		static bool testHResult(HRESULT hr)
 		{
 			if (!SUCCEEDED(hr)) {
@@ -51,6 +123,14 @@ namespace AudioDeviceApi
 			}
 		};
 
+		/**
+		 * Converts a EDataFlow to the corresponding DeviceType::Enum.
+		 * \param dataFlow the EDataFlow to convert to DeviceType::Enum
+		 * \return a DeviceType::Enum, in accordance with the following:
+		 *           * if dataFlow is eRender, return DeviceType::PLAYBACK
+		 *           * if dataFlow is eCapture, return DeviceType::RECORDING
+		 *           * if dataflow is anything else, return DeviceType::UNKNOWN
+		 */
 		static DeviceType::Enum DataFlow2DeviceType(EDataFlow dataflow)
 		{
 			switch (dataflow) {
@@ -65,7 +145,6 @@ namespace AudioDeviceApi
 		
 
 	protected:
-		//HRESULT hr;
 		IMMDeviceEnumerator* pDeviceEnum;
 		IMMDeviceCollection* pDeviceCollection;
 		EDataFlow dataFlow;
